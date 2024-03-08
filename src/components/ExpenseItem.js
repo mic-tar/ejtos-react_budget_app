de82a0c3c89b28080671dc4221e0dfa0ce39bd93
mic-tar <162429370+mic-tar@users.noreply.github.com>
2024-03-08 16:30:00 +0100

    refactor: add decrease button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+
+import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
+
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -24,12 +27,25 @@ const ExpenseItem = (props) => {
         });
 
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
 
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><FaPlusCircle size='1.9em' color='green' onClick={event=> increaseAllocation(props.name)}>+</FaPlusCircle></td>
+        <td><FaMinusCircle size='1.9em' color='darkred' onClick={event=> decreaseAllocation(props.name)}>+</FaMinusCircle></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
